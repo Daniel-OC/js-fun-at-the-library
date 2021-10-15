aada5705541b2f35958eab75d5843aba222072fd
Daniel O'Connell <danieloconnell.maker@gmail.com>
2021-10-15 16:11:18 -0700

    Complete shelfBook

--- src/shelf.js
+++ src/shelf.js
@@ -1,6 +1,13 @@
+function shelfBook(book, shelf) {
+  if (book.genre = "sciFi" && shelf.length <=2){
+    shelf.unshift(book)
+  }
+  //console.log(shelf);
+}
+
 
 module.exports = {
-  // shelfBook,
+  shelfBook,
   // unshelfBook,
   // listTitles,
   // searchShelf
