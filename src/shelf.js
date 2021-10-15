e93c02f78d44fc48afb59cbac657443ec56e9e13
Daniel O'Connell <danieloconnell.maker@gmail.com>
2021-10-15 16:38:18 -0700

    Complete unshelfBook

--- src/shelf.js
+++ src/shelf.js
@@ -1,14 +1,25 @@
 function shelfBook(book, shelf) {
-  if (book.genre = "sciFi" && shelf.length <=2){
+  if (book.genre = "sciFi" && shelf.length < 3){
     shelf.unshift(book)
   }
   //console.log(shelf);
 }
 
+function unshelfBook(title, shelf) {
+  //console.log(shelf);
+  for (i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+      shelf.splice(i,1)
+    }
+  }
+
+}
+
+
 
 module.exports = {
   shelfBook,
-  // unshelfBook,
+  unshelfBook,
   // listTitles,
   // searchShelf
 };
