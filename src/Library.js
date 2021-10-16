f52ca6e49d0ca35183b909338587b5430fac9110
Daniel O'Connell <danieloconnell.maker@gmail.com>
2021-10-16 12:31:53 -0700

    Solve addBook

--- src/Library.js
+++ src/Library.js
@@ -11,8 +11,19 @@ function createLibrary(name) {
   return(library)
 }
 
+function addBook(branch, book) {
+  if (book.genre === "fantasy") {
+    branch.shelves.fantasy.push(book)
+  } else if (book.genre === "nonFiction") {
+    branch.shelves.nonFiction.push(book)
+  } else if (book.genre === "fiction") {
+    branch.shelves.fiction.push(book)
+  }
+
+}
+
 module.exports = {
   createLibrary,
-  // addBook,
+  addBook,
   // checkoutBook
 };
